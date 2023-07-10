47770ad37faafa704cccd617a0aff7a7f96524b6
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-07-11 01:38:51 +0300

    2 hours and 30 minutes in react 18 toturial 12 hours course youtube

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-    
-    return <h1>hello from react js</h1>
+function BookList() {
+  return (
+    <section className="book-list">
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        image={firstBook.image}
+      />
+      <Book
+        title={secondtBook.title}
+        author={secondtBook.author}
+        image={secondtBook.image}
+      />
+    </section>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting/>)
+const firstBook = {
+  title: "first book title",
+  author: "ashraf sayed 2023",
+  image: "./images/book.jpg",
+};
+const secondtBook = {
+  title: "second book title",
+  author: "mohammed ali",
+  image: "./images/book2.jpeg",
+};
 
+const Book = (props) => {
+  const { author, title, image } = props;
+  return (
+    <article className="book">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
