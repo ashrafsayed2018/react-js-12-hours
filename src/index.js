2c042eb5cb2d37fc485cb45cca055f3cf6d0695e
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-07-21 02:23:18 +0300

    till the 3 hours of 12 hours corse code addict channel

--- src/index.js
+++ src/index.js
@@ -2,41 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const books = [
+  {
+    id: 1,
+    title: "first book title",
+    author: "ashraf sayed 2023",
+    image: "./images/book.jpg",
+  },
+  {
+    id: 2,
+    title: "second book title",
+    author: "mohammed ali",
+    image: "./images/book2.jpeg",
+  },
+];
+
 function BookList() {
   return (
     <section className="book-list">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        image={firstBook.image}
-      />
-      <Book
-        title={secondtBook.title}
-        author={secondtBook.author}
-        image={secondtBook.image}
-      />
+      {books.map((book) => {
+        // const { id, author, title, image } = book;
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 }
 
-const firstBook = {
-  title: "first book title",
-  author: "ashraf sayed 2023",
-  image: "./images/book.jpg",
-};
-const secondtBook = {
-  title: "second book title",
-  author: "mohammed ali",
-  image: "./images/book2.jpeg",
-};
-
 const Book = (props) => {
   const { author, title, image } = props;
+  console.log(props);
   return (
     <article className="book">
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button>click me</button>
     </article>
   );
 };
